c9a3fb8c05ddeefb8960ae068d7b20c8f205f7ca
Lex1s20 <alexrubets335@gmail.com>
2024-12-03 13:56:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projekt3/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.projekt3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projekt3.ui.theme.PROJEKT3Theme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val fs = Firebase.firestore
+        fs.collection("books")
+            .document().set(mapOf("name" to "My book"))
+    }
+}
+
